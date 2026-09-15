12cf2d271bc3d4b55eecc3956544cd90584c8f01
Ruhi K <ruhik8789@gmail.com>
2026-09-15 23:55:14 +0530

    Added global error handler.

--- server/src/app.js
+++ server/src/app.js
@@ -3,11 +3,16 @@ const cors = require("cors");
 
 const interviewRoutes = require("./routes/interviewRoutes");
 
+const errorHandler = require("./middleware/errorHandler");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+
 app.use("/api/interviews", interviewRoutes);
+
+app.use(errorHandler);
 
 module.exports = app;
